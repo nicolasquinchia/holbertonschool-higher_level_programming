bfbd9f18450150b001bd6f90e022e0816f60294b
nicolasquinchia <nicolasquinchia@gmail.com>
2020-04-28 21:34:21 +0000

    0x00, C task cycle on linked list

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+/**
+ * check_cycle - Check if there is a cycle in a linked list
+ * @list: linked list head
+ *
+ * Return: 1 if find a cycle otherwise 0
+ */
+int check_cycle(listint_t *list)
+{
+	listint_t *temp = list;
+	listint_t *curr = list;
+
+	while (temp != '\0' && temp->next != '\0')
+	{
+		curr = curr->next;
+		temp = temp->next;
+		temp = temp->next;
+		if (temp == curr)
+		{
+			return (1);
+		}
+	}
+	return (0);
+}
