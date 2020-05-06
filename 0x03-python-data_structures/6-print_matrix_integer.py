02b318d0dee2106fd5fb68264f5624ac892076e5
nicolasquinchia <nicolasquinchia@gmail.com>
2020-05-06 04:09:10 +0000

    0x03, print matrix

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    if matrix == [[]]:
+        print("")
+    else:
+        for x in matrix:
+            for i in range(0, len(x)):
+                if i < len(x) - 1:
+                    print("{:d}".format(x[i]), end=" ")
+                else:
+                    print("{:d}".format(x[i]))
