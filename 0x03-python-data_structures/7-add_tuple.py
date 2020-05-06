2c1bcabc4a9a7a88b9cf75b739666c4b64917030
nicolasquinchia <nicolasquinchia@gmail.com>
2020-05-06 04:38:55 +0000

    0x03 add tuple

---
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def add_tuple(tuple_a=(), tuple_b=()):
+    tuple_a = tuple_a + (0, 0)
+    tuple_b = tuple_b + (0, 0)
+    x = tuple_a[0] + tuple_b[0]
+    y = tuple_a[1] + tuple_b[1]
+    add = (x, y)
+    return add
