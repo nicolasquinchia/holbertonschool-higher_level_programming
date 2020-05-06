b0d69e534c27ea0e5ba2e45bd618eee586819b5e
nicolasquinchia <nicolasquinchia@gmail.com>
2020-05-06 05:42:41 +0000

    0x03 if is divisible by 2, change identation

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
-    new_str = ""
+    new_str = []
     for x in my_list:
         if x % 2 == 0:
             new_str.append(True)
         else:
             new_str.append(False)
-        return new_str
+    return new_str
