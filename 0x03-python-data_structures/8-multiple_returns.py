30376056820082fa44ebbebe9be6fdd020a8fa05
nicolasquinchia <nicolasquinchia@gmail.com>
2020-05-06 04:49:24 +0000

    0x03 returns a tuple with the length of a string and its first character, change if condition

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    if sentence == False:
+    if not sentence:
         return len(sentence), None
     else:
         return len(sentence), sentence[0]
