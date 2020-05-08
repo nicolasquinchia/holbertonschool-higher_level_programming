9aac3347bfdf1ca8e9087327124b60201d69bd4f
nicolasquinchia <nicolasquinchia@gmail.com>
2020-05-08 04:17:49 +0000

    0x04 pow list with map, just one line

--- 0x04-python-more_data_structures/101-square_matrix_map.py
+++ 0x04-python-more_data_structures/101-square_matrix_map.py
@@ -1,6 +1,3 @@
 #!/usr/bin/python3
 def square_matrix_map(matrix=[]):
-    new_mtx = []
-    for i in matrix:
-        new_mtx.append(list(map(lambda x: x ** 2, i)))
-    return new_mtx
+    return [list(map(lambda x: x**2, n)) for n in matrix]
