57598e8db268807f2fafe434b1d097d7800ba422
nicolasquinchia <nicolasquinchia@gmail.com>
2020-05-20 00:18:57 -0500

    0x06, add possition space, delete try in init method

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -18,13 +18,8 @@ class Square:
     """
 
     def __init__(self, size=0, position=(0, 0)):
-        if type(size) != int:
-            raise TypeError("size must be an integer")
-        elif size < 0:
-            raise ValueError("size must be >= 0")
-        else:
-            self.__size = size
-            self.__position = position
+        self.__size = size
+        self.__position = position
 
     """
     Method to return the area of the square.
