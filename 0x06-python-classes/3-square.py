c2b76cae51d7c006e5680e1608e749a2d5b5c1ba
nicolasquinchia <nicolasquinchia@gmail.com>
2020-05-19 23:29:40 -0500

    0x06, return area of the squeare

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""Class Square"""
+
+
+class Square:
+    """Class Square that defines a square.
+    Attributes:
+        size(int) - instance private: The size of a square
+    Erros:
+        TypeError - must be an integer
+        ValueError - must be greather than zero
+    """
+
+    """
+    Method to initialize an object or instance
+    and check if there are any kind of error.
+    """
+
+    def __init__(self, size=0):
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+    """
+    Method to return the area of the square.
+    """
+
+    def area(self):
+        return self.__size * self.__size
