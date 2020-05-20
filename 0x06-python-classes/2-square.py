e60740de90660a587ba2b5f96e068bfab49c6507
nicolasquinchia <nicolasquinchia@gmail.com>
2020-05-19 23:25:21 -0500

    0x06, secure initialize an object

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""2- Size validation"""
+
+
+class Square:
+    """Class Square that defines a square.
+    Attributes:
+        size - instance private: The size of a square
+    Erros:
+        TypeError - must be an integer
+        ValueError - must be greather than zero
+    """
+
+    """
+    Method to initialize an object or instance
+    and check if there are any kind of error.
+    """
+
+    def __init__(self, size=0):
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
