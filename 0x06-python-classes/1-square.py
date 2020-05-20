e2b39be254cf00a7013ed222bd3bb8abb1abcae1
nicolasquinchia <nicolasquinchia@gmail.com>
2020-05-19 23:22:56 -0500

    0x06, initialize an object

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Class Square"""
+
+
+class Square:
+    """Class Square that defines a square.
+    attributes:
+        size: Variable that represents the size of the square.
+    """
+
+    # method to initialize an object
+    def __init__(self, size):
+        self.__size = size
