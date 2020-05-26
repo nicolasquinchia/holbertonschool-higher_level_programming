350f6a22a02ca236ba2e3edda926734b6c5408ab
nicolasquinchia <nicolasquinchia@gmail.com>
2020-05-25 20:17:48 -0500

    0x08, Rectangle class with area and perimeter, add perimeter condition

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -81,4 +81,7 @@ class Rectangle:
         Returns:
             [int] -- Rectangle perimeter
         """
-        return ((self.width + self.height) * 2)
+        if self.width == 0 or self.height == 0:
+            return 0
+        else:
+            return ((self.width + self.height) * 2)
