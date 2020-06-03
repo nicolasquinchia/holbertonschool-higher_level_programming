985bafa37af090c1f1935ad18598a2dd1bd8491f
nicolasquinchia <nicolasquinchia@gmail.com>
2020-06-02 21:43:03 -0500

    0x0A, print a sorted new list with doctest

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Module that holds MyList class to sort a list
+    """
+
+
+class MyList(list):
+    """ class that inherits from list
+
+    Arguments:
+        list {[list]} -- Parent class
+    """
+
+    def print_sorted(self):
+        """Prints a new list sorted based on the object intanced
+        """
+        print(sorted(self))
