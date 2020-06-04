0f36667927850a842e6a39e5000142a52790511f
nicolasquinchia <nicolasquinchia@gmail.com>
2020-06-03 22:30:52 -0500

    0x0B, dict description from an obj

---
+++ 0x0B-python-input_output/10-class_to_json.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""This module returns a object
+    as a dict.
+    """
+import json
+
+
+def class_to_json(obj):
+    """funtion that returns the dict description with simple data structure 
+
+    Arguments:
+        obj {[obj]} -- Object to convert into a data structure
+
+    Returns:
+        [dict] -- Simple data structure from the obj
+    """
+    return obj.__dict__
