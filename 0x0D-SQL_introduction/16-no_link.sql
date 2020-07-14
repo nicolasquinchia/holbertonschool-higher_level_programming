c67f99bae7b7d7ce3775bdd807cf037616961982
nicolasquinchia <nicolasquinchia@gmail.com>
2020-07-13 22:58:45 -0500

    0x0D, select records in a specific order and specific columns, check if the name value is not null, add the semi colon, check documentation

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,7 +1,4 @@
--- lists all records of the table second_table of the database hbtn_0c_0 in your MySQL server.
--- Don’t list rows without a name value
--- Results should display the score and the name (in this order)
--- Records should be listed by descending score
+-- lists some records of the table second_table of the database, no Null names, desc order
 SELECT score, name,
     FROM second_table
     WHERE name IS NOT NULL
