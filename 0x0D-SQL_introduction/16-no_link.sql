c8a86846257fa56abee661c189a3ef2eaac6252b
nicolasquinchia <nicolasquinchia@gmail.com>
2020-07-13 23:00:05 -0500

    0x0D, select records in a specific order and specific column delete a colon

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,5 +1,5 @@
 -- lists some records of the table second_table of the database, no Null names, desc order
-SELECT score, name,
+SELECT score, name
     FROM second_table
     WHERE name IS NOT NULL
     ORDER BY score DESC;
