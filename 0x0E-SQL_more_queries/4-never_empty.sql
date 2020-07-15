ee9bc4c1b3298fe53fce39ef64607b73702b1fdf
nicolasquinchia <nicolasquinchia@gmail.com>
2020-07-14 20:26:29 -0500

    0x0E, Create a table on current database with a default value, check table name

--- 0x0E-SQL_more_queries/4-never_empty.sql
+++ 0x0E-SQL_more_queries/4-never_empty.sql
@@ -1,4 +1,4 @@
 -- Creates the table id_not_null on current MySQL server.
-CREATE TABLE IF NOT EXISTS force_name(
+CREATE TABLE IF NOT EXISTS id_not_null(
     id INT NOT NULL DEFAULT 1,
     name VARCHAR(256));
