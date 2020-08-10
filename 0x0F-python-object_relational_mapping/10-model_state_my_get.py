d70c5c45dde3b51d498c3c584b6543b770657ebc
nicolasquinchia <nicolasquinchia@gmail.com>
2020-08-09 20:30:54 -0500

    0x0F, Model State filter by one element using argv

---
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Prints the State object with the name passed
+as argument from the database hbtn_0e_6_usa
+    """
+from model_state import Base, State
+from sqlalchemy import (create_engine)
+from sys import argv
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        argv[1], argv[2], argv[3]), pool_pre_ping=True)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    state = session.query(State).filter(State.name == argv[4]).first()
+    print(state)
+    if state:
+        print("{}".format(state.id))
+    else:
+        print("Not found")
