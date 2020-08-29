5f5b797fc3dcec58fd9abc8491ad18720afe48d4
nicolasquinchia <nicolasquinchia@gmail.com>
2020-08-29 18:40:35 -0500

    0x11, Python, Network 1, 0 fetches a URL, check pep8

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
-import urllib.request
 """Module that fetches https://intranet.hbtn.io/status
     """
+import urllib.request
+
 if __name__ == "__main__":
     with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
         html = response.read()
