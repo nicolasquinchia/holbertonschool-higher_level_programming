9f862ec7c01818187404c9821ffb99e69ef860f8
nicolasquinchia <nicolasquinchia@gmail.com>
2020-08-30 18:26:17 -0500

    0x11, post email, ad permissions

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -11,7 +11,9 @@ import urllib.parse
 if __name__ == "__main__":
     url = sys.argv[1]
     values = {'email': sys.argv[2]}
-    data = urllib.parse.urlencode(values).encode('utf-8')
+    data = urllib.parse.urlencode(values)
+    data = data.encode('utf-8')
     req = urllib.request.Request(url, data)
     with urllib.request.urlopen(req) as response:
-        print(response.read().decode('utf-8'))
+        html = response.read()
+        print(html.decode('utf-8'))
