445e3527c0238f02db9ff96b7e1ce50dbcad2cd5
nicolasquinchia <nicolasquinchia@gmail.com>
2020-08-30 18:21:26 -0500

    0x11, post email, change defode to utf-8

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -11,9 +11,7 @@ import urllib.parse
 if __name__ == "__main__":
     url = sys.argv[1]
     values = {'email': sys.argv[2]}
-    data = urllib.parse.urlencode(values)
-    data = data.encode('utf-8')
+    data = urllib.parse.urlencode(values).encode('utf-8')
     req = urllib.request.Request(url, data)
     with urllib.request.urlopen(req) as response:
-        html = response.read()
-        print(html.decode('utf-8'))
+        print(response.read().decode('utf-8'))
