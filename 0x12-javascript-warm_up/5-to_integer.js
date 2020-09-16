05f9302f07fbeb7fe11d1df72aed8b88b01d2344
nicolasquinchia <nicolasquinchia@gmail.com>
2020-09-15 22:43:59 -0500

    0x12, task 3 value argument, task 4 concat, task 5 to integer using parseInt

---
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+const args = process.argv;
+const myInt = parseInt(args[2]);
+if (isNaN(myInt)) {
+  console.log('Not a number');
+} else {
+  console.log('My number: ' + myInt);
+}
