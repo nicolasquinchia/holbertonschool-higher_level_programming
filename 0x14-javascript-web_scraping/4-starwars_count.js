df3838e24af0eb5dfb895bdae9f4edbdc7392d01
nicolasquinchia <nicolasquinchia@gmail.com>
2020-09-22 22:48:07 -0500

    delete an unused variable task 4

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,14 +1,16 @@
 #!/usr/bin/node
 const request = require('request');
-const toFind = 'https://swapi-api.hbtn.io/api/people/18/';
+const toFind = '/18/';
 request(process.argv[2], function (error, response, body) {
   if (error) {
     console.error(error);
   }
   let movies = 0;
   for (const movie of JSON.parse(body).results) {
-    if (movie.characters.includes(toFind)) {
-      movies += 1;
+    for (const person of movie.characters) {
+      if (person.slice(-4) === toFind) {
+        movies += 1;
+      }
     }
   }
   console.log(movies);
