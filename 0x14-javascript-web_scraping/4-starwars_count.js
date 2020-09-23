b1f097263c0dc5383209b407a62b9c10826d1a5d
nicolasquinchia <nicolasquinchia@gmail.com>
2020-09-22 21:27:23 -0500

    0x14, tasks 4, change how to count the number of ocurrences, and delete the else

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -5,13 +5,12 @@ const characUrl = 'https://swapi-api.hbtn.io/api/people/18/';
 let movies = 0;
 request(requestUrl, function (error, response, body) {
   if (error) {
-    console.log(error);
-  } else {
-    const toObj = JSON.parse(body);
-    for (let i = 0; toObj.results[i]; i++) {
-      if (toObj.results[i].characters.includes(characUrl)) {
-        movies++;
-      }
+    console.error(error);
+  }
+  const toObj = JSON.parse(body);
+  for (let i = 0; toObj.results[i]; i++) {
+    if (toObj.results[i].characters.includes(characUrl)) {
+      movies++;
     }
   }
   console.log(movies);
